d64ccc6f32d8716dc09faa85bc207eb1b8b50d9e
Weeke <baileydunlap9@gmail.com>
2023-12-22 02:03:58 -0500

    Update sscan.py

--- sscan.py
+++ sscan.py
@@ -1,4 +1,4 @@
-import aiohttp, asyncio, argparse, logging, signal, socket, string, sys
+import aiohttp, asyncio, asyncio, argparse, logging, signal, socket, string, sys
 
 class SubdomainScanError(Exception):
     pass
@@ -12,57 +12,46 @@ class AdvancedSubdomainScanner:
         self.output_file = output_file
         self.resolver = aiohttp.resolver.AsyncResolver()
 
-        logging.basicConfig(level=logging.INFO)
-        self.logger = logging.getLogger(__name__)
-
-    async def resolve_subdomain(self, subdomain):
+    async def resolve_subdomain(self, session, subdomain):
         try:
-            async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(resolver=self.resolver)) as session:
-                async with session.get(f'http://{subdomain}.{self.domain}') as response:
-                    ip = socket.gethostbyname(subdomain)
-                    self.logger.info(f'{subdomain}.{self.domain} ({ip})')
+            async with session.get(f'http://{subdomain}.{self.domain}') as response:
+                ip = socket.gethostbyname(subdomain)
+                logging.info(f'{subdomain}.{self.domain} ({ip})')
         except (aiohttp.ClientConnectorError, socket.gaierror) as e:
-            self.logger.error(f"Error resolving {subdomain}.{self.domain}: {e}")
+            logging.error(f"Error resolving {subdomain}.{self.domain}: {e}")
 
-    async def scan_subdomains(self, subdomains):
-        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(resolver=self.resolver)):
-            tasks = []
-            for subdomain in subdomains:
-                task = asyncio.ensure_future(self.resolve_subdomain(subdomain))
-                tasks.append(task)
-                if len(tasks) >= self.concurrency:
-                    await asyncio.gather(*tasks)
-                    tasks.clear()
-            if tasks:
-                await asyncio.gather(*tasks)
+    async def scan_subdomains(self, session, subdomains):
+        tasks = [self.resolve_subdomain(session, subdomain) for subdomain in subdomains]
+        await asyncio.gather(*tasks)
 
-    async def scan_wordlist(self):
+    async def scan_wordlist(self, session):
         try:
             async with open(self.wordlist, 'r') as file:
                 subdomains = [line.strip() for line in file]
-            await self.scan_subdomains(subdomains)
+            await self.scan_subdomains(session, subdomains)
         except FileNotFoundError as e:
             raise SubdomainScanError(f"Wordlist file not found: {self.wordlist}")
 
-    async def brute_force(self):
+    async def brute_force(self, session):
         if self.length_range:
             min_length, max_length = self.length_range
             for length in range(min_length, max_length + 1):
-                await self._brute_force_recursive('', length)
+                await self._brute_force_recursive(session, '', length)
 
-    async def _brute_force_recursive(self, current, remaining_length):
+    async def _brute_force_recursive(self, session, current, remaining_length):
         if remaining_length == 0:
-            await self.scan_subdomains([current])
+            await self.scan_subdomains(session, [current])
         else:
             for char in string.ascii_lowercase + string.digits:
-                await self._brute_force_recursive(current + char, remaining_length - 1)
+                await self._brute_force_recursive(session, current + char, remaining_length - 1)
 
     async def run_scans(self):
-        if self.wordlist:
-            await self.scan_wordlist()
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(resolver=self.resolver)) as session:
+            if self.wordlist:
+                await self.scan_wordlist(session)
 
-        if self.length_range:
-            await self.brute_force()
+            if self.length_range:
+                await self.brute_force(session)
 
 def main():
     parser = argparse.ArgumentParser(description="Advanced Subdomain Scanner")
@@ -73,6 +62,8 @@ def main():
     parser.add_argument("-r", "--range", type=int, nargs=2, help="Length range for brute force (e.g. '3 5')")
     parser.add_argument("-o", "--output", help="Output file for results")
     args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO)
 
     scanner = AdvancedSubdomainScanner(args.domain, args.wordlist, args.concurrency, args.range, args.output)
 
